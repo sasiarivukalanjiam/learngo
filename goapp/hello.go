66cd9c999b380f18080456d15a09c3e7de41a9f9
vicky18Dec <devid.abhishek@gmail.com>
2018-03-29 21:25:05 +0530

    go webapp

---
+++ goapp/hello.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+        "fmt"
+	        "net/http"
+		)
+
+		func Hello(w http.ResponseWriter, req *http.Request) {
+		                fmt.Fprintln(w, "Hello world!")
+				        }
+
+					func main() {
+					        http.HandleFunc("/", Hello)
+						        fmt.Println("Starting up on 80")
+							        log.Fatal(http.ListenAndServe(":80", nil))
+								}
+
