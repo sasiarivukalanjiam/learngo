20e9bd1343c48167ecf1c15012d510939fb94192
Abhishek <employ.abhishek@gmail.com>
2018-03-29 21:32:47 +0530

    Update hello.go

--- goapp/hello.go
+++ goapp/hello.go
@@ -2,16 +2,15 @@ package main
 
 import (
         "fmt"
-	        "net/http"
-		)
+	"net/http"
+)
 
-		func Hello(w http.ResponseWriter, req *http.Request) {
-		                fmt.Fprintln(w, "Hello world!")
-				        }
+func Hello(w http.ResponseWriter, req *http.Request) {
+        fmt.Fprintln(w, "Hello world!")
+}
 
-					func main() {
-					        http.HandleFunc("/", Hello)
-						        fmt.Println("Starting up on 80")
-							        log.Fatal(http.ListenAndServe(":80", nil))
-								}
-
+func main() {
+        http.HandleFunc("/", Hello)
+	fmt.Println("Starting up on 80")
+	log.Fatal(http.ListenAndServe(":80", nil))
+}
